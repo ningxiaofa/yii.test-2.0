7e5c7502318e1aee367d1e21216bcc0459cadd1a
william ning <1158885641@qq.com>
2022-09-17 18:21:39 +0800

    Console下控制器的用法

---
+++ commands/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+class IndexController extends Controller
+{
+
+    public function actionIndex()
+    {
+        echo __FILE__;
+
+        // ➜  yii.test git:(master) ✗ php72 yii index/index
+        // /Users/huangbaoyin/Documents/Env/docker-lnmp-dev-env-sh/html/yii.test/commands/IndexController.php%                                
+        // ➜  yii.test git:(master) ✗ 
+
+    }
+}
