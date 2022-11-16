b1a5933597bc92aba17c4463f74bffa9e04254e8
william ning <1158885641@qq.com>
2022-11-16 19:54:00 +0800

    yii -- cli

--- commands/IndexController.php
+++ commands/IndexController.php
@@ -16,4 +16,27 @@ class IndexController extends Controller
         // ➜  yii.test git:(master) ✗ 
 
     }
+
+    //  php yii index/argv name=ningxiaofa age=30
+    public function actionArgv($argv, $argv1)
+    {
+        var_dump($argv, $argv1);
+
+        // 输出结果：
+        // ➜  yii2.test git:(main) ✗ php yii index/argv name=ningxiaofa age=30
+        // string(15) "name=ningxiaofa"
+        // string(6) "age=30"
+        // ➜  yii2.test git:(main) ✗ 
+    }
+
+    //  php yii index/list m=123 abc xyz
+    public function actionList($argv, $argv1, $argv2)
+    {
+        var_dump($argv, $argv1, $argv2);
+        // ➜  yii2.test git:(main) ✗ php yii index/list m=123 abc xyz
+        // string(5) "m=123"
+        // string(3) "abc"
+        // string(3) "xyz"
+        // ➜  yii2.test git:(main) ✗ 
+    }
 }
